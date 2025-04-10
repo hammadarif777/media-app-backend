476ea03aba0243ffc66135706db85c1f1ee0c065
HammadArif777 <arifhammad4@gmail.com>
2025-04-11 01:17:47 +0500

    initial commit

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
 // index.js
 import express from "express";
-import connectDB from "./src/db/index.js";
+import connectDB from "./src/config/index.js";
 import dotenv from "dotenv";
 
-// Load environment variables early
 dotenv.config();
 
 const app = express();
 
-// Constants should be uppercase for clarity
 const PORT = process.env.PORT || 3001;
 
-// Middleware (optional, can be expanded later)
-app.use(express.json()); // For parsing JSON requests
+app.use(express.json());
 
-// Async function to initialize app
 const startServer = async () => {
   try {
-    // Wait for DB connection before starting server
     await connectDB();
-    // Start server only after DB is connected
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
     console.error("Failed to start server:", error);
-    process.exit(1); // Exit if server fails to start
+    process.exit(1);
   }
 };
 
-// Start the server
 startServer();
